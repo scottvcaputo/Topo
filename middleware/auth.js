8b677d0e4070ab0396118585fe425ecdc20480ed
Scott Caputo <55561456+scottvcaputo@users.noreply.github.com>
2020-10-25 23:39:56 -0400

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+module.exports = {
+    ensureAuth: function (req, res, next) {
+      if (req.isAuthenticated()) {
+        return next()
+      } else {
+        res.redirect('/')
+      }
+    },
+    ensureGuest: function (req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      } else {
+        res.redirect('/dashboard');
+      }
+    },
+  }
+
+// this is to ensure that you can't access the website beyond the home page without signing in 
